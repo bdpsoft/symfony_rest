be36fe27a2c697465732459a9206653e21554593
Draža <bdpsoft@gmail.com>
2019-10-11 12:31:10 +0200

    ubacen security

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface
